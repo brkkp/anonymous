7783ff53277b432791f516c42a0e618363143574
brkkp <brksockets@tuta.com>
2024-07-06 16:03:27 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,20 +13,9 @@ const userMovement = document.getElementById('user--movement')
 const userDate = document.getElementById('user--date')
 const userDaysPassed = document.getElementById('user--days-passed')
 //
-let dateNew = new Date('2024')
-const overlay = document.createElement('div')
-overlay.classList.add('overlay', 'loading')
-overlay.innerHTML = `<svg class="icon icon--spinner light icon--48">
-<use xlink:href="./assets/sprite.svg#icon-spinner"></use></svg>`
-const dayTxt = document.createElement('p')
-dayTxt.classList.add('day')
-dayTxt.innerHTML = `${daysPassed(dateNew)}`
-body.prepend(dayTxt)
-contentButtonClose.remove()
-
-const optionAll = {
+const option = {
   day: 'numeric',
-  month: 'numeric',
+  month: 'long',
   hour: 'numeric',
   minute: 'numeric',
   second: 'numeric',
@@ -43,90 +32,165 @@ const optionClock = {
   second: 'numeric',
 }
 
+const acc1 = {
+  n: 'J',
+  d0: [200, 150],
+  d1: [new Date(2024, 5, 4), new Date('2020')],
+  en: 1101,
+}
+
+const acc2 = {
+  n: 'P',
+  d0: [700, 12250, 200],
+  d1: [new Date(2024, 5, 4), new Date(2024, 3, 4), new Date('2024')],
+  en: 2262,
+}
+const acc3 = {
+  n: 'P',
+  d0: [700, 12250, 200],
+  d1: [...acc2.d1],
+  en: 1445,
+}
+const acc4 = {
+  n: 'E',
+  d0: [200, 150],
+  d1: [...acc1.d1],
+  en: 9245,
+}
+const acc5 = {
+  n: 'O',
+  d0: [200, 150, 700, 1450, 100, 53],
+  d1: [...acc1.d1],
+  en: 7284,
+}
+const acc6 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc7 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc8 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc9 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc10 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc11 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc12 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc13 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc14 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+const acc15 = {
+  n: 'F',
+  d0: [200],
+  d1: [new Date(2021, 5, 4)],
+  en: 1245,
+}
+
+const accounts = [
+  acc1,
+  acc2,
+  acc3,
+  acc4,
+  acc5,
+  acc6,
+  acc7,
+  acc8,
+  acc9,
+  acc10,
+  acc11,
+  acc12,
+  acc13,
+  acc14,
+  acc15,
+]
+
+let day = new Date('2024')
+const overlay = document.createElement('div')
+overlay.classList.add('overlay', 'loading')
+overlay.innerHTML = `<svg class="icon icon--spinner light icon--48">
+<use xlink:href="./assets/sprite.svg#icon-spinner"></use></svg>`
+const dayTxt = document.createElement('p')
+dayTxt.classList.add('day')
+dayTxt.innerHTML = `${daysPassed(day)}`
+body.prepend(dayTxt)
+contentButtonClose.remove()
+
 setInterval(() => {
-  dateNew = new Date()
+  let dateNew = new Date()
   const dateFormat = new Intl.DateTimeFormat('it', optionClock).format(dateNew)
   date.innerHTML = dateFormat
 }, 1000)
 
-const person1 = {
-  nick: 'J',
-  debit: [200, 150],
-  date: [new Date(2024, 5, 4), new Date('2020')],
-  code: 1101,
-}
-
-const person2 = {
-  nick: 'P',
-
-  debit: [700, 12250, 200],
-  date: [new Date(2024, 5, 4), new Date(2024, 3, 4), new Date('2024')],
-  code: 2262,
-}
-const person3 = {
-  nick: 'P',
-
-  debit: [700, 12250, 200],
-  date: [...person2.date],
-  code: 1445,
-}
-const person4 = {
-  nick: 'E',
-
-  debit: [200, 150],
-  date: [...person1.date],
-  code: 9245,
-}
-const person5 = {
-  nick: 'O',
-
-  debit: [200, 150, 700, 1450, 100, 53],
-  date: [...person1.date],
-  code: 7284,
-}
-const person6 = {
-  nick: 'F',
-
-  debit: [200],
-  date: [new Date(2021, 5, 4)],
-  code: 1245,
-}
-
-const accounts = [person1, person2, person3, person4, person5, person6]
+//
 for (const [i, el] of accounts.entries()) {
   contentBoxFirst.innerHTML += `
   <div role="button" class="content__note content__note--${i}" data-ver="${i}">
-  <p class="details details--name">${el.nick}</p>
-  <p class="details details--name">${el.code}</p>
+  <p class="details details--name">${el.n}</p>
+  <p class="details details--name">${el.en}</p>
   </div>`
 }
 
-let casualSiz = [500, 800, 900, 1050, 500]
+//
 contentBoxFirst.addEventListener('click', function (e) {
-  let num = Math.trunc(Math.random() * casualSiz.length)
+  e.preventDefault()
   const id = e.target.closest('.content__note')
   if (!id) return
   const selectedAcc = accounts[id.getAttribute('data-ver')]
-
   //
   contentBoxSecond.classList.add('active')
   contentBoxSecond.prepend(overlay)
-  setTimeout(() => {
-    overlay.remove()
-    contentButtonClose.classList.remove('d-none')
-    contentEl.prepend(contentButtonClose)
-  }, casualSiz[num])
+  contentButtonClose.classList.remove('d-none')
+  contentEl.prepend(contentButtonClose)
+  loading()
   //
-  userName.innerHTML = `${selectedAcc.nick}`
-  userCode.innerHTML = `${selectedAcc.code}`
+  userName.innerHTML = `${selectedAcc.n}`
+  userCode.innerHTML = `${selectedAcc.en}`
 
   document.querySelector('.content__body').innerHTML = ''
-  for (const [i] of selectedAcc.debit.entries()) {
+  for (const [i] of selectedAcc.d0.entries()) {
     document.querySelector('.content__body').innerHTML += `
     <div class="content__card">
-      <p class="details" id="user--movement">${selectedAcc.debit[i]}</p>
+      <p class="details" id="user--movement">${selectedAcc.d0[i]}</p>
       <p class="details" id="user--days-passed">${daysPassed(
-        selectedAcc.date[i]
+        selectedAcc.d1[i]
       )} giorni fa.</p>
     </div>
     `
@@ -137,15 +201,19 @@ contentBoxFirst.addEventListener('click', function (e) {
     .querySelectorAll('.content__note')
     .forEach(e => e.classList.remove('active'))
   id.classList.add('active')
-
-  //
-  // if (contentBoxSecond.classList.contains('active')) {
-  //   contentEl.prepend(contentButtonClose)
-  // }
 })
 
 contentButtonClose.addEventListener('click', reset)
+
+///
 ////
+//////
+function loading() {
+  setTimeout(() => {
+    overlay.remove()
+  }, 500)
+}
+
 function reset() {
   contentButtonClose.remove()
   contentBoxSecond.classList.remove('active')
